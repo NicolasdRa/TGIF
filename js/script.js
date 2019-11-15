a88f3a2794b92560e757181fd711b2287f1fcf1f
Nicolas <nicolasdirago@gmail.com>
2019-11-15 08:00:45 +0100

    14-Nov

--- js/script.js
+++ js/script.js
@@ -6,10 +6,12 @@ $('.materialboxed').materialbox()
 let title = document.title
 
 if (title === "Senate Members" || title === "House Members") {
-    runFillTableCode()
+    fillMainTables()
+} else if (title === "Senate Attendance") {
+    fillAtGlanceTable()
 }
 
-function runFillTableCode() {
+function fillMainTables() {
 
     // Selects Table Bodies
     const tableBody = document.querySelector("tbody");
@@ -46,7 +48,7 @@ function runFillTableCode() {
             fullName = member.firstName + " " + member.middleName + " " + member.lastName
         };
 
-        // Creates data cells
+        // Creates data cells 
         td1.innerHTML = fullName
         td2.innerHTML = member.party
         td3.innerHTML = member.state
@@ -62,5 +64,87 @@ function runFillTableCode() {
 
         // Appends rows
         tableBody.appendChild(tr);
+    })
+}
+
+function fillAtGlanceTable() {
+
+    // Selects Table Bodies
+    const tableBodyatGlance = document.querySelector("#At-Glance-Table > tbody")
+
+    // Creates array with data
+    let parties = [
+
+        {
+            name: "Democrats",
+            noRep: houseStatistics.Democrats,
+            pl: houseStatistics.plDemocrats
+        },
+
+        {
+            name: "Republicans",
+            noRep: houseStatistics.Republicans,
+            pl: houseStatistics.plRepublicans
+        },
+
+        {
+            name: "Independents",
+            noRep: houseStatistics.Independents,
+            pl: "null"
+        },
+
+        {
+            name: "Totals",
+            noRep: houseStatistics.Democrats + houseStatistics.Republicans + houseStatistics.Independents,
+            pl: ((houseStatistics.plDemocrats + houseStatistics.plRepublicans)/2)        }
+
+    ]
+
+    // Creates Table elements and fills content
+    parties.forEach((party) => {
+
+        const tr = document.createElement("tr");
+
+        const td1 = document.createElement("td");
+        const td2 = document.createElement("td");
+        const td3 = document.createElement("td");
+
+
+        // Creates data cells
+        td1.innerHTML = party.name
+        td2.innerHTML = party.noRep
+        td3.innerHTML = Math.round( party.pl * 1e2 ) / 1e2
+
+        
+        // // OPTION 2
+
+        // let parties = ["Democrats", "Republicans", "Independents", "Totals"]
+
+
+        // if (party === "Democrats") {
+        //     td1.innerHTML = parties[0]
+        //     td2.innerHTML = houseStatistics.Democrats
+        //     td3.innerHTML = houseStatistics.plDemocrats
+        // } else if (party === "Republicans") {
+        //     td1.innerHTML = parties[1]
+        //     td2.innerHTML = houseStatistics.Republicans
+        //     td3.innerHTML = houseStatistics.plRepublicans
+        // } else if (party === "Independents") {
+        //     td1.innerHTML = parties[2]
+        //     td2.innerHTML = houseStatistics.Independents
+        //     td3.innerHTML = houseStatistics.plIndependents
+        // } else if (party === "Totals") {
+        //     td1.innerHTML = parties[3]
+        //     td2.innerHTML = houseStatistics.Republicans + houseStatistics.Republicans + houseStatistics.Independents
+        //     td3.innerHTML = houseStatistics.plRepublicans + houseStatistics.plRepublicans + houseStatistics.plIndependents
+        // }
+
+        // Appends cells
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+
+        // Appends rows
+        tableBodyatGlance.appendChild(tr);
     })
 }
--- js/statistics.js
+++ js/statistics.js
@@ -1,14 +1,4 @@
-let houseStatistics = {
-    Democrats: 0,
-    Republicans: 0,
-    Independents: 0,
-    plDemocrats: 0,
-    plRepublicans: 0,
-    mostEngaged: "{null}",
-    leastEngaged: "null",
-    mostMissers: "null",
-    leastMissers: "null"
-};
+// Creates variable members according to page title
 
 let members
 if (document.title === "Senate Members") {
@@ -17,34 +7,75 @@ if (document.title === "Senate Members") {
     members = dataHouse.results[0].members;
 }
 
-function addMembers(mem) {
+// CREATES one new array for each party
+
+let demArray = []
+let repArray = []
+let indArray = []
+
+function createMemberArrays(mem) {
+
     if (mem.party === "D") {
-        houseStatistics.Democrats++
+        demArray.push(mem)
     } else if (mem.party === "R") {
-        houseStatistics.Republicans++
+        repArray.push(mem)
     } else if (mem.party === "I") {
-        houseStatistics.Independents++
+        indArray.push(mem)
     }
-};
+}
+
+// // passes number of members directly into the housestatistics object // WORKS
+// function splitMembers(mem) {
+//     if (mem.party === "D") {
+//         houseStatistics.Democrats++
+//     } else if (mem.party === "R") {
+//         houseStatistics.Republicans++
+//     } else if (mem.party === "I") {
+//         houseStatistics.Independents++
+//     }
+// };
+
+// PARTY LOYALTY calculations
+
+let plDem = 0
+let plRep = 0
+
+function getAverages() {
+
+    demArray.forEach((member) => {
+        plDem += member.votes_with_party_pct;
+    })
+    
+    repArray.forEach((member) => {
+        plRep  += member.votes_with_party_pct;
+    })
+}
 
 
 function runStats() {
 
-    let total = 0
-
     members.forEach((member) => {
-        addMembers(member)
-
-        total = total + member.votes_with_party_pct
-
+        createMemberArrays(member)
     })
-
-    var average = total / members.length
-
-    houseStatistics.plDemocrats = average
 }
 
+runStats()
+getAverages()
+
+
+// OBJECT statistics
+
+let houseStatistics = {
+
+    Democrats: demArray.length,
+    Republicans: repArray.length,
+    Independents: indArray.length,
+    plDemocrats: (plDem / demArray.length),
+    plRepublicans: (plRep / repArray.length),
+    mostEngaged: "{null}",
+    leastEngaged: "null",
+    mostMissers: "null",
+    leastMissers: "null"
+};
 
 console.log("satistics", houseStatistics);
-
-runStats()
