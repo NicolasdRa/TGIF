1da542c8799d5ffafe19faaf35004d6fd989b501
Nicolas <nicolasdirago@gmail.com>
2019-11-26 05:17:13 +0100

    broken code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//Necessary for Materialize drop down menu, accordion, checkboxes
+// Necessary for Materialize drop down menu, accordion, checkboxes
 $(".dropdown-trigger").dropdown()
 $('.collapsible').collapsible()
 $('.materialboxed').materialbox()
@@ -9,13 +9,12 @@ $('select').formSelect()
 var title = document.title
 
 var url;
-if (title === "Senate Members") {
+if (title === "Senate Members" || title === "Senate Attendance" || title === "Senate Loyalty") {
     url = 'https://api.propublica.org/congress/v1/116/senate/members.json'
-} else if (title === "House Members") {
+} else if (title === "House Members" || title === "House Attendance" || title === "House Loyalty") {
     url = 'https://api.propublica.org/congress/v1/116/house/members.json'
 }
 
-var members = []
 
 // FETCH CONTROLER
 if (title === "Senate Members" || title === "House Members" || title === "Senate Attendance" || title === "House Attendance" || title === "Senate Loyalty" || title === "House Loyalty") {
@@ -31,14 +30,39 @@ if (title === "Senate Members" || title === "House Members" || title === "Senate
             return response.json();
         })
         .then(function (data) {
-            (data.results[0].members).forEach(member => {
-                members.push(member)
-            });
-            fillTables()
+            //(data.results[0].members).forEach(member => {
+            //   members.push(member)
+            // });
+            var members = data.results[0].members
+
+            fillTables(members)
+
+            //document.querySelector(".preloader-wrapper big active").style.display ='none';
+            $('#loader').addClass("hide-loader");
 
         })
         .catch(function (error) {
             console.log(error)
         });
+}
 
+function fillTables(members) {
+
+    if (title === "Senate Members" || title === "House Members") {
+        runCheckboxFilter()
+        runDdFilter(members)
+        fillMainTable(members)
+
+    } else if (title === "Senate Attendance" || title === "House Attendance") {
+        runStats(members)
+        fillaAtGlanceTable()
+        fillLeastEngagedTable(members)
+        fillMostEngagedTable(members)
+
+    } else if (title === "Senate Loyalty" || title === "House Loyalty") {
+        runStats(members)
+        fillaAtGlanceTable()
+        fillLeastLoyalTable(members)
+        fillMostLoyalTable(members)
+    }
 }
--- js/tables.js
+++ js/tables.js
@@ -1,21 +1,5 @@
-function fillTables() {
-
-    if (title === "Senate Members" || title === "House Members") {
-        checkBoxesValues()
-        filterByState()
-        getSelectedText()
-        fillMainTable()
-
-    } else if (title === "Senate Attendance" || title === "House Attendance") {
-        fillaAtGlanceTable()
-        fillLeastEngagedTable()
-        fillMostEngagedTable()
-
-    } else if (title === "Senate Loyalty" || title === "House Loyalty") {
-        fillaAtGlanceTable()
-        fillLeastLoyalTable()
-        fillMostLoyalTable()
-    }
+// RUNS ALL STATISTICS / GLobal
+function runStats(members) {
 
     // CREATES  new array for each party
 
@@ -33,7 +17,7 @@ function fillTables() {
         }
     }
 
-    // PARTY LOYALTY calculations
+    // PARTY LOYALTY calculations / Global
     let plWithDem = 0
     let plWithRep = 0
     let plAgainstDem = 0
@@ -52,16 +36,11 @@ function fillTables() {
     }
     getAverages()
 
-    // ENGAGEMENT calculations
 
-
-    // RUNS ALL STATISTICS
-    function runStats() {
-        members.forEach((member) => {
-            createPartyArrays(member)
-        })
-    }
-    runStats()
+    // runs all stats
+    members.forEach((member) => {
+        createPartyArrays(member)
+    })
 
 
     // Statistics OBJECT
@@ -78,13 +57,19 @@ function fillTables() {
         leastEngaged: "null",
     };
 
+    console.log(statistics)
 
-    // FILTER FUNCTION FOR CHECKBOXES
+    statistics.Democrats = demArray.length
+    statistics.Republicans = repArray.length
+    statistics.Independents = indArray.length
+}
+
+function runCheckboxFilter() {
+    // Filter by Party Function (checkboxes)
 
     // Event listener
     var checkBoxSelection = document.querySelector('.checkboxes')
     checkBoxSelection.addEventListener('change', fillMainTable)
-
 
     // Returns array of Values from Checked Checkboxes
     function checkBoxesValues() {
@@ -96,118 +81,448 @@ function fillTables() {
         return array
     }
 
+    // Filter Variables Checkbox / Dropdown
+    const tableBody = document.querySelector("tbody");
+    var checkBoxes = checkBoxesValues()
+    var ddmenu = getSelectedText()
 
-    // FILTER FUNCTION FOR STATES
+}
 
-    function filterByState() {
 
-        // Creates array of States
-        var states = []
-        members.forEach((state) => {
-            states.push(state.state)
-        });
+// Filter by State
+function runDdFilter(members) {
 
-        // Removes duplicates
-        states = states.filter(function (state, index) {
-            return states.indexOf(state) === index;
-        })
+    // Creates array of States
+    var states = []
+    members.forEach((state) => {
+        states.push(state.state)
+    });
 
-        // Sorts array
-        states.sort()
+    // Removes duplicates
+    states = states.filter(function (state, index) {
+        return states.indexOf(state) === index;
+    })
 
-        // Adds "All options"
-        states.splice(0, 0, 'ALL STATES');
+    // Sorts array
+    states.sort()
 
-        // Fills OPTIONS with State Names
-        const ddmenu = document.querySelector('.browser-default');
+    // Adds "All options"
+    states.splice(0, 0, 'ALL STATES');
 
-        states.forEach((i) => {
-            var option = document.createElement("option");
+    // Fills OPTIONS with State Names
+    const ddmenu = document.querySelector('.browser-default');
 
-            // Fills cell Data 
-            option.innerHTML = [i]
+    states.forEach((i) => {
+        var option = document.createElement("option");
 
-            // Appends cells
-            ddmenu.appendChild(option);
-        })
+        // Fills cell Data 
+        option.innerHTML = [i]
 
-        // Event Listener
-        ddmenu.addEventListener('change', fillMainTable);
-    }
+        // Appends cells
+        ddmenu.appendChild(option);
+    })
 
-    //Gets selected text from options
+    // Event Listener
+    ddmenu.addEventListener('change', fillMainTable);
+
+    //Gets selected text from Dropdown Menu options
     function getSelectedText() {
         const ddmenu = document.querySelector('.browser-default');
         return ddmenu.options[ddmenu.selectedIndex].text;
     }
+    getSelectedText()
+}
 
 
-    // FILLS MAIN TABLES WITH MEMBERS
+// FILLS MAIN TABLES WITH MEMBERS
+function fillMainTable(members) {
 
-    function fillMainTable() {
+    // Clears Tables before printing members
+    tableBody.innerHTML = ""
 
-        // Variables Checkbox Filter
-        const tableBody = document.querySelector("tbody");
-        var checkBoxes = checkBoxesValues()
-        var ddmenu = getSelectedText()
+    // Creates Tables and fills content   
+    members.forEach((member) => {
 
-        // Clears Tables before printing members
-        tableBody.innerHTML = ""
+        // Checks if Party Name is included in the array before creating element
+        if ((ddmenu === 'ALL STATES' || ddmenu === 'Choose State') && (checkBoxes.includes(member.party))) {
+            insertMembers()
+        } else if (checkBoxes.includes(member.party) && (ddmenu === (member.state))) {
+            insertMembers()
+        }
 
-        // Creates Tables and fills content   
-        members.forEach((member) => {
-            
-            // Checks if Party Name is included in the array before creating element
-            if ((ddmenu === 'ALL STATES' || ddmenu === 'Choose State') && (checkBoxes.includes(member.party))) {
-                insertMembers()
-            } else if (checkBoxes.includes(member.party) && (ddmenu === (member.state))) {
-                insertMembers()
-            }
+        function insertMembers() {
+            const tr = document.createElement("tr");
+            const td1 = document.createElement("td");
+            const td2 = document.createElement("td");
+            const td3 = document.createElement("td");
+            const td4 = document.createElement("td");
+            const td5 = document.createElement("td");
 
-            function insertMembers() {
+            // Creates Full-Name Field
+            var firstName = member.first_name
+            var middleName = member.middle_name
+            var lastName = member.last_name
 
-                const tr = document.createElement("tr");
+            var fullName
+            if (middleName === null) {
+                fullName = firstName + " " + lastName
+            } else {
+                fullName = firstName + " " + middleName + " " + lastName
+            };
 
-                const td1 = document.createElement("td");
-                const td2 = document.createElement("td");
-                const td3 = document.createElement("td");
-                const td4 = document.createElement("td");
-                const td5 = document.createElement("td");
+            // Creates Link
+            var a = document.createElement('a');
+            a.href = member.url;
 
-                // Creates Full-Name Field
-                var firstName = member.first_name
-                var middleName = member.middle_name
-                var lastName = member.last_name
+            // Creates data cells 
+            a.innerHTML = fullName
+            td1.appendChild(a)
+            td2.innerHTML = member.party
+            td3.innerHTML = member.state
+            td4.innerHTML = member.seniority
+            td5.innerHTML = member.votes_with_party_pct
 
-                var fullName
-                if (middleName === null) {
-                    fullName = firstName + " " + lastName
-                } else {
-                    fullName = firstName + " " + middleName + " " + lastName
-                };
+            // Appends cells
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            tr.appendChild(td3);
+            tr.appendChild(td4);
+            tr.appendChild(td5);
 
-                // Creates Link
-                var a = document.createElement('a');
-                a.href = member.url;
+            // Appends rows
+            tableBody.appendChild(tr);
+        }
+    })
+}
 
-                // Creates data cells 
-                a.innerHTML = fullName
-                td1.appendChild(a)
-                td2.innerHTML = member.party
-                td3.innerHTML = member.state
-                td4.innerHTML = member.seniority
-                td5.innerHTML = member.votes_with_party_pct
+// FILLS AT A GLANCE TABLES
 
-                // Appends cells
-                tr.appendChild(td1);
-                tr.appendChild(td2);
-                tr.appendChild(td3);
-                tr.appendChild(td4);
-                tr.appendChild(td5);
+function fillaAtGlanceTable() {
+    // Selects Table Bodies
+    const tableBodyatGlance = document.querySelector("#At_a_Glance")
 
-                // Appends rows
-                tableBody.appendChild(tr);
-            }
-        })
-    }
+    // Creates array with Party Data for At a Glance Tables
+    let parties = [{
+            name: "Democrats",
+            noRep: statistics.Democrats,
+            pl: statistics.plDemocrats
+        },
+
+        {
+            name: "Republicans",
+            noRep: statistics.Republicans,
+            pl: statistics.plRepublicans
+        },
+
+        {
+            name: "Independents",
+            noRep: statistics.Independents,
+            pl: "null"
+        },
+
+        {
+            name: "Totals",
+            noRep: statistics.Democrats + statistics.Republicans + statistics.Independents,
+            pl: ((statistics.plDemocrats + statistics.plRepublicans) / 2)
+        }
+    ]
+
+
+    // Creates Table elements and fills content
+    parties.forEach((party) => {
+
+        const tr = document.createElement("tr");
+        const td1 = document.createElement("td");
+        const td2 = document.createElement("td");
+        const td3 = document.createElement("td");
+
+        // Creates data cells
+        td1.innerHTML = party.name
+        td2.innerHTML = party.noRep
+        td3.innerHTML = Math.round(party.pl * 1e2) / 1e2
+
+        // Appends cells
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+
+        // Appends rows
+        tableBodyatGlance.appendChild(tr);
+    })
+}
+
+
+function fillLeastEngagedTable(members) {
+
+    var sortedMembers = members.slice();
+    sortedMembers.sort((a, b) => {
+        return b.missed_votes_pct - a.missed_votes_pct;
+    });
+
+
+    // CREATES FIRST 10% 
+
+    var cutLength = Math.round((members.length) * 0.1)
+    var splicedMembers = sortedMembers.splice(0, cutLength)
+    var lastMember10prc = splicedMembers[splicedMembers.length - 1]
+
+    // ADDS DUPPLICATES
+    sortedMembers.forEach((member) => {
+        if (member.missed_votes_pct === lastMember10prc.missed_votes_pct) {
+            if (lastMember10prc != member)
+                splicedMembers.push(member)
+        }
+    })
+
+
+    // FILL TABLES
+    const tableLeast = document.querySelector('#Least-Engaged');
+
+    splicedMembers.forEach((member) => {
+
+        const tr = document.createElement("tr");
+
+        const td1 = document.createElement("td");
+        const td2 = document.createElement("td");
+        const td3 = document.createElement("td");
+
+
+        // Creates Full-Name Field
+        var firstName = member.first_name
+        var middleName = member.middle_name
+        var lastName = member.last_name
+
+        var fullName
+        if (middleName === null) {
+            fullName = firstName + " " + lastName
+        } else {
+            fullName = firstName + " " + middleName + " " + lastName
+        };
+
+        // Creates Link
+        var a = document.createElement('a');
+        a.href = member.url;
+
+        // Creates data cells 
+        a.innerHTML = fullName
+        td1.appendChild(a)
+
+        td2.innerHTML = member.missed_votes
+        td3.innerHTML = member.missed_votes_pct
+
+        // Appends cells
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+
+        // Appends rows
+        tableLeast.appendChild(tr);
+    })
+}
+
+function fillMostEngagedTable(members) {
+
+    var sortedMembersMost = members.slice();
+    sortedMembersMost.sort((a, b) => {
+        return a.missed_votes_pct - b.missed_votes_pct;
+    });
+
+    // CREATES FIRST 10% 
+
+    var cutLength = Math.round((members.length) * 0.1)
+    var splicedMembersMost = sortedMembersMost.splice(0, cutLength)
+    var lastMember10prc = splicedMembersMost[splicedMembersMost.length - 1]
+
+    // ADDS DUPPLICATES
+    sortedMembersMost.forEach((member) => {
+        if (member.missed_votes_pct === lastMember10prc.missed_votes_pct) {
+            if (lastMember10prc != member)
+                splicedMembersMost.push(member)
+        }
+    })
+
+
+    // FILL TABLES
+    const tableMost = document.querySelector('#Most-Engaged');
+
+    splicedMembersMost.forEach((member) => {
+
+        const tr = document.createElement("tr");
+
+        const td1 = document.createElement("td");
+        const td2 = document.createElement("td");
+        const td3 = document.createElement("td");
+
+
+        // Creates Full-Name Field
+        var firstName = member.first_name
+        var middleName = member.middle_name
+        var lastName = member.last_name
+
+        var fullName
+        if (middleName === null) {
+            fullName = firstName + " " + lastName
+        } else {
+            fullName = firstName + " " + middleName + " " + lastName
+        };
+
+        // Creates Link
+        var a = document.createElement('a');
+        a.href = member.url;
+
+
+        // Creates data cells 
+        a.innerHTML = fullName
+        td1.appendChild(a)
+
+        td2.innerHTML = member.missed_votes
+        td3.innerHTML = member.missed_votes_pct
+
+        // Appends cells
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+
+        // Appends rows
+        tableMost.appendChild(tr);
+    })
+}
+
+function fillLeastLoyalTable(members) {
+
+    var sortedMembers = members.slice();
+    sortedMembers.sort((a, b) => {
+        return b.votes_against_party_pct - a.votes_against_party_pct;
+    });
+
+
+    // CREATES FIRST 10% 
+
+    var cutLength = Math.round((members.length) * 0.1)
+    var splicedMembers = sortedMembers.splice(0, cutLength)
+    var lastMember10prc = splicedMembers[splicedMembers.length - 1]
+
+
+
+    // ADDS DUPPLICATES
+
+    sortedMembers.forEach((member) => {
+        if (member.votes_against_party_pct === lastMember10prc.votes_against_party_pct) {
+            splicedMembers.push(member)
+            if (lastMember10prc != member)
+                splicedMembers.push(member)
+        }
+    })
+
+
+    // FILL TABLES
+
+    const tableLeast = document.querySelector('#Least-Loyal');
+
+    splicedMembers.forEach((member) => {
+
+        const tr = document.createElement("tr");
+
+        const td1 = document.createElement("td");
+        const td2 = document.createElement("td");
+        const td3 = document.createElement("td");
+
+
+        // Creates Full-Name Field
+        var firstName = member.first_name
+        var middleName = member.middle_name
+        var lastName = member.last_name
+
+        var fullName
+        if (middleName === null) {
+            fullName = firstName + " " + lastName
+        } else {
+            fullName = firstName + " " + middleName + " " + lastName
+        };
+
+        // Creates Link
+        var a = document.createElement('a');
+        a.href = member.url;
+
+        // Creates data cells 
+        a.innerHTML = fullName
+        td1.appendChild(a)
+
+        td2.innerHTML = member.total_votes
+        td3.innerHTML = member.votes_with_party_pct
+
+        // Appends cells
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+
+        // Appends rows
+        tableLeast.appendChild(tr);
+    })
+}
+
+function fillMostLoyalTable(members) {
+
+    var sortedMembersMost = members.slice();
+    sortedMembersMost.sort((a, b) => {
+        return b.votes_with_party_pct - a.votes_with_party_pct;
+    });
+
+
+    // CREATES FIRST 10% 
+    var cutLength = Math.round((members.length) * 0.1)
+    var splicedMembersMost = sortedMembersMost.splice(0, cutLength)
+    var lastMember10prc = splicedMembersMost[splicedMembersMost.length - 1]
+
+
+    // ADDS DUPPLICATES
+    sortedMembersMost.forEach((member) => {
+        if (member.votes_with_party_pct === lastMember10prc.votes_with_party_pct) {
+            splicedMembersMost.push(member)
+            if (lastMember10prc != member)
+                splicedMembersMost.push(member)
+        }
+    })
+
+
+    // FILL TABLES
+    const tableMost = document.querySelector('#Most-Loyal');
+
+    splicedMembersMost.forEach((member) => {
+        const tr = document.createElement("tr");
+        const td1 = document.createElement("td");
+        const td2 = document.createElement("td");
+        const td3 = document.createElement("td");
+
+        // Creates Full-Name Field
+        var firstName = member.first_name
+        var middleName = member.middle_name
+        var lastName = member.last_name
+
+        var fullName
+        if (middleName === null) {
+            fullName = firstName + " " + lastName
+        } else {
+            fullName = firstName + " " + middleName + " " + lastName
+        };
+
+        // Creates Link
+        var a = document.createElement('a');
+        a.href = member.url;
+
+        // Creates data cells 
+        a.innerHTML = fullName
+        td1.appendChild(a)
+
+        td2.innerHTML = member.total_votes
+        td3.innerHTML = member.votes_with_party_pct
+
+        // Appends cells
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+
+        // Appends rows
+        tableMost.appendChild(tr);
+    })
 }
