9c34a7b5702e6f9cbb00d62ed491859b37b0e6d3
Nicolas <nicolasdirago@gmail.com>
2020-07-04 19:43:11 +0200

    Styling - Started Refactoring with Materialize - Phone First Approach

--- js/main.js
+++ js/main.js
@@ -1,72 +1,77 @@
 // Necessary for Materialize drop down menu, accordion, checkboxes
-$(".dropdown-trigger").dropdown()
+$('.dropdown-trigger').dropdown()
 $('.collapsible').collapsible()
 $('.materialboxed').materialbox()
 $('select').formSelect()
+$('.sidenav').sidenav()
 
-
-// Main variables 
+// Main variables
 var title = document.title
-var membersGlobal 
-var url;
-if (title === "Senate Members" || title === "Senate Attendance" || title === "Senate Loyalty") {
-    url = 'https://api.propublica.org/congress/v1/116/senate/members.json'
-} else if (title === "House Members" || title === "House Attendance" || title === "House Loyalty") {
-    url = 'https://api.propublica.org/congress/v1/116/house/members.json'
+var membersGlobal
+var url
+if (
+  title === 'Senate Members' ||
+  title === 'Senate Attendance' ||
+  title === 'Senate Loyalty'
+) {
+  url = 'https://api.propublica.org/congress/v1/116/senate/members.json'
+} else if (
+  title === 'House Members' ||
+  title === 'House Attendance' ||
+  title === 'House Loyalty'
+) {
+  url = 'https://api.propublica.org/congress/v1/116/house/members.json'
 }
-
 
 // FETCH CONTROLER
-if (title === "Senate Members" || title === "House Members" || title === "Senate Attendance" || title === "House Attendance" || title === "Senate Loyalty" || title === "House Loyalty") {
+if (
+  title === 'Senate Members' ||
+  title === 'House Members' ||
+  title === 'Senate Attendance' ||
+  title === 'House Attendance' ||
+  title === 'Senate Loyalty' ||
+  title === 'House Loyalty'
+) {
+  // DATA REQUEST
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      'X-API-Key': 'zgVuv018iIYLQCaUljwf3zgKx6h7cd2MlsT1pHsT'
+    }
+  })
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      var members = data.results[0].members
+      membersGlobal = members
+      functionController(members)
 
-
-    // DATA REQUEST
-    fetch(url, {
-            method: "GET",
-            headers: {
-                "X-API-Key": "zgVuv018iIYLQCaUljwf3zgKx6h7cd2MlsT1pHsT"
-            }
-        })
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-
-            var members = data.results[0].members
-            membersGlobal = members
-            functionController(members)
-            
-            document.querySelector('#loader').style.display ='none';
-            // $('#loader').addClass("hide-loader");
-
-        })
-        .catch(function (error) {
-            console.log(error)
-        });
+      document.querySelector('#loader').style.display = 'none'
+      // $('#loader').addClass("hide-loader");
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
 }
 
-function functionController(members) {
-
-    if (title === "Senate Members" || title === "House Members") {
-        getSelectedText()
-        checkBoxSelected()
-        runDropdownFilter(members)
-        fillMainTable(members)
-        runEventListeners(members)
-        insertMembers(members)
-      
-    } else if (title === "Senate Attendance" || title === "House Attendance") {
-        
-        runStats(members)
-        fillaAtGlanceTable()
-        fillLeastEngagedTable(members)
-        fillMostEngagedTable(members)
-
-    } else if (title === "Senate Loyalty" || title === "House Loyalty") {
-      
-        runStats(members)
-        fillaAtGlanceTable()
-        fillLeastLoyalTable(members)
-        fillMostLoyalTable(members)
-    }
+function functionController (members) {
+  if (title === 'Senate Members' || title === 'House Members') {
+    getSelectedText()
+    checkBoxSelected()
+    runDropdownFilter(members)
+    fillMainTable(members)
+    runEventListeners(members)
+    insertMembers(members)
+  } else if (title === 'Senate Attendance' || title === 'House Attendance') {
+    runStats(members)
+    fillaAtGlanceTable()
+    fillLeastEngagedTable(members)
+    fillMostEngagedTable(members)
+  } else if (title === 'Senate Loyalty' || title === 'House Loyalty') {
+    runStats(members)
+    fillaAtGlanceTable()
+    fillLeastLoyalTable(members)
+    fillMostLoyalTable(members)
+  }
 }
